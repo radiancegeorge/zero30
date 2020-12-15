552f975e42fbbe0a7909b6050b907d4c8b286d4c
radiancegeorge <radiancegeorge@outlook.com>
2020-12-16 00:32:17 +0100

    completed sign up

--- js/sign up drawer.js
+++ js/sign up drawer.js
@@ -1,8 +1,20 @@
 window.addEventListener('load', e=>{
     const form = document.querySelector('header form');
     const drawer = document.querySelector('header .sign-up');
+    const cancelForm = document.querySelector('.cancel-form');
 
     drawer.addEventListener('click', e=>{
+        form.classList.toggle('away');
+        if(form.classList.contains('away') === false){
+            drawer.style.backgroundColor = 'white';
+            drawer.firstChild.style.color = 'black';
+
+        }else{
+            drawer.style.backgroundColor = '';
+            drawer.firstChild.style.color = '';
+        }
+    });
+    cancelForm.addEventListener('click', e=>{
         form.classList.toggle('away');
         if(form.classList.contains('away') === false){
             drawer.style.backgroundColor = 'white';
