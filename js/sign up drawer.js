074477e252f86a1c462800a478d26debfe01170b
radiancegeorge <radiancegeorge@outlook.com>
2020-12-18 13:50:03 +0100

    fixed index sign up

--- js/sign up drawer.js
+++ js/sign up drawer.js
@@ -1,28 +1,37 @@
 window.addEventListener('load', e=>{
     const form = document.querySelector('header form');
-    const drawer = document.querySelector('header .sign-up');
+    const drawer_header = document.querySelector('header .sign-up');
+    const drawer2 = document.querySelector('.get-started .sign-up');
+    const allDrawer = [drawer_header, drawer2]
     const cancelForm = document.querySelector('.cancel-form');
 
-    drawer.addEventListener('click', e=>{
-        form.classList.toggle('away');
-        if(form.classList.contains('away') === false){
-            drawer.style.backgroundColor = 'white';
-            drawer.firstChild.style.color = 'black';
-
-        }else{
-            drawer.style.backgroundColor = '';
-            drawer.firstChild.style.color = '';
-        }
-    });
+    allDrawer.forEach(drawer=>{
+        drawer.addEventListener('click', e=>{
+            form.classList.toggle('away');
+            if(form.classList.contains('away') === false){
+                window.scrollTo({
+                    top:0,
+                    left:0,
+                    behavior:'smooth'
+                })
+                drawer_header.style.backgroundColor = 'white';
+                drawer_header.firstChild.style.color = 'black';
+    
+            }else{
+                drawer_header.style.backgroundColor = '';
+                drawer_header.firstChild.style.color = '';
+            }
+        });
+    })
     cancelForm.addEventListener('click', e=>{
         form.classList.toggle('away');
         if(form.classList.contains('away') === false){
-            drawer.style.backgroundColor = 'white';
-            drawer.firstChild.style.color = 'black';
+            drawer_header.style.backgroundColor = 'white';
+            drawer_header.firstChild.style.color = 'black';
 
         }else{
-            drawer.style.backgroundColor = '';
-            drawer.firstChild.style.color = '';
+            drawer_header.style.backgroundColor = '';
+            drawer_header.firstChild.style.color = '';
         }
     })
 })
