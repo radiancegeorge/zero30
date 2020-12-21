60ff36d478fdc98c0f03a8938b4d67e31fdc77a4
radiancegeorge <radiancegeorge@outlook.com>
2020-12-21 21:30:05 +0100

    td

--- js/data table.js
+++ js/data table.js
@@ -15,4 +15,29 @@ window.addEventListener('load', e=>{
             select: false
         })
     }
+    if($('.admin-user-table')){
+        const table = $('.admin-user-table');
+        table.DataTable({
+            searching: false,
+            ordering: false,
+            select: false
+        })
+    };
+    if($('.admin-transactions')){
+        const table = $('.admin-transactions');
+        table.DataTable({
+            searching: false,
+            ordering: false,
+            select: false
+        })
+    };
+    if($('.admin-products')){
+        const table = $('.admin-products');
+        table.DataTable({
+            searching: false,
+            ordering: false,
+            select: false
+        })
+    };
+
 })
