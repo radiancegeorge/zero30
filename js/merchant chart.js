9ed981733f7cdf94727c4dc5725b5f2d9c933e59
radiancegeorge <radiancegeorge@outlook.com>
2020-12-21 01:59:23 +0100

    merchant dashboard

--- js/merchant chart.js
+++ js/merchant chart.js
@@ -6,7 +6,7 @@ const chart = new Chart(ctx, {
         datasets: [{
             label: '',//asign a label in here
             data: [
-                0, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000
+                0, 10000, 20000, 30000, 40000, 10000, 60000, 70000, 20000, 90000, 100000
             ],
             borderColor:[
                 '#E31A1C',
@@ -16,7 +16,73 @@ const chart = new Chart(ctx, {
                 '#B2DF8A',
                 '#A6CEE3'
             ],
-            borderWidth: 1,
+            backgroundColor: 'transparent',
+            lineColor: 'red',
+            borderWidth: 4,
+        },{
+            label: '',//asign a label in here
+            data: [
+                0, 1000, 20000, 60000, 50000, 10000, 60000, 7000, 20000, 7100, 10000
+            ],
+            borderColor:[
+                '#E31A1C',
+                '#1F78B4',
+                '#33A02C',
+                '#FB9A99',
+                '#B2DF8A',
+                '#A6CEE3'
+            ],
+            backgroundColor: 'transparent',
+            borderWidth: 4,
+        
+        },{
+            label: '',//asign a label in here
+            data: [
+                0, 10000, 20000, 30000, 10000, 10000, 60000, 70000, 20000, 70000, 10000
+            ],
+            borderColor:[
+                '#E31A1C',
+                '#1F78B4',
+                '#33A02C',
+                '#FB9A99',
+                '#B2DF8A',
+                '#A6CEE3'
+            ],
+            backgroundColor: 'transparent',
+            borderWidth: 4,
+        
+        },{
+            label: '',//asign a label in here
+            data: [
+                8000, 10000, 20000, 30000, 4000, 10000, 60000, 4000, 20000, 70000, 10000
+            ],
+            borderColor:[
+                '#E31A1C',
+                '#1F78B4',
+                '#33A02C',
+                '#FB9A99',
+                '#B2DF8A',
+                '#A6CEE3'
+            ],
+            backgroundColor: 'transparent',
+            borderWidth: 4,
+        
+        },{
+            label: '',//asign a label in here
+            data: [
+                0, 10000, 20000, 30000, 8000, 10000, 60000, 7000, 20000
+            ],
+            borderColor:[
+                '#E31A1C',
+                '#1F78B4',
+                '#33A02C',
+                '#FB9A99',
+                '#B2DF8A',
+                '#A6CEE3'
+            ],
+            backgroundColor: 'transparent',
+            borderWidth: 4,
+        
         }]
         
     }       
