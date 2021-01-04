1f68da53ea8e5e5ae5a8836f083fadd67ee50210
radiancegeorge <radiancegeorge@outlook.com>
2021-01-04 22:32:42 +0100

    fixed dtable

--- js/data table.js
+++ js/data table.js
@@ -2,81 +2,81 @@ window.addEventListener('load', e=>{
     if($('.ongoing-orders-table')){
         const table = $('.ongoing-orders-table');
         table.DataTable({
-            searching: false,
-            ordering: false,
-            select: false
+            searching: true,
+            ordering: true,
+            select: true
         })
     }
     if($('.pending-orders-table')){
         const table = $('.pending-orders-table');
         table.DataTable({
-            searching: false,
-            ordering: false,
-            select: false
+            searching: true,
+            ordering: true,
+            select: true
         })
     }
     if($('.admin-user-table')){
         const table = $('.admin-user-table');
         table.DataTable({
-            searching: false,
-            ordering: false,
-            select: false
+            searching: true,
+            ordering: true,
+            select: true
         })
     };
     if($('.admin-transactions')){
         const table = $('.admin-transactions');
         table.DataTable({
-            searching: false,
-            ordering: false,
-            select: false
+            searching: true,
+            ordering: true,
+            select: true
         })
     };
     if($('.admin-products')){
         const table = $('.admin-products');
         table.DataTable({
-            searching: false,
-            ordering: false,
-            select: false
+            searching: true,
+            ordering: true,
+            select: true
         })
     };
     if($('.my-merchants')){
         const table = $('.my-merchants');
         table.DataTable({
-            searching: false,
-            ordering: false,
-            select: false
+            searching: true,
+            ordering: true,
+            select: true
         })
     };
     if($('.manage-riders')){
         const table = $('.manage-riders');
         table.DataTable({
-            searching: false,
-            ordering: false,
-            select: false
+            searching: true,
+            ordering: true,
+            select: true
         })
     };
     if($('.merchant-user-table')){
         const table = $('.merchant-user-table');
         table.DataTable({
-            searching: false,
-            ordering: false,
-            select: false
+            searching: true,
+            ordering: true,
+            select: true
         })
     };
     if($('.merchant-products')){
         const table = $('.merchant-products');
         table.DataTable({
-            searching: false,
-            ordering: false,
-            select: false
+            searching: true,
+            ordering: true,
+            select: true
         })
     };
     if($('.merchant-orders-table')){
         const table = $('.merchant-orders-table');
         table.DataTable({
-            searching: false,
-            ordering: false,
-            select: false
+            searching: true,
+            ordering: true,
+            select: true
         })
     };
 
